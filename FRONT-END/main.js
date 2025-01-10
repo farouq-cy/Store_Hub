d7ffff87d0e7b2b4dded93b080a1505ddcd4cf15
MagedEmel <magedemel007@gmail.com>
2025-01-10 16:30:50 +0200

    s

--- FRONT-END/main.js
+++ FRONT-END/main.js
@@ -5,24 +5,27 @@ const sunIcon = document.querySelector(".toggle .bxs-sun");
 const moonIcon = document.querySelector(".toggle .bx-moon");
 
 toggle.addEventListener("change", () => {
-    
-    body.classList.toggle("dark");
-    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-    if(body.classList.contains("dark")){
-        localStorage.setItem("DarkToogle", "Dark");
-    } else{
-        localStorage.removeItem("DarkToogle");
-    }
+  body.classList.toggle("dark");
+  sunIcon.className =
+    sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+  moonIcon.className =
+    moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("DarkToogle", "Dark");
+  } else {
+    localStorage.removeItem("DarkToogle");
+  }
 });
 document.addEventListener("DOMContentLoaded", () => {
-    if(localStorage.getItem("DarkToogle")){
-        body.classList.add("dark");
-        sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-        moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-    }
-})
+  if (localStorage.getItem("DarkToogle")) {
+    body.classList.add("dark");
+    sunIcon.className =
+      sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className =
+      moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+  }
+});
 
 document.getElementById("showLinks").addEventListener("click", () => {
-    document.getElementById("showLinks").classList.toggle("active");
-})
+  document.getElementById("showLinks").classList.toggle("active");
+});
