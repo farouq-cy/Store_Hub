33a196dc95abb5d48a2272b9f58bdf4d84397f58
devfady1 <fadya5323@gmail.com>
2025-02-08 16:09:53 +0000

    منلعبش فحاجه بقا

--- BACK-END/STORE_HUB/store_hub/settings.py
+++ BACK-END/STORE_HUB/store_hub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,13 +34,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'grappelli',
     'pages.apps.PagesConfig',
-    #'pages.apps.SuitConfig',
+    #'pages.apps.SuitConfig',from theam
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'templates',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'store_hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
