d806cbb06c6b13d25793d337b3fb26e5ed5a8f4a
devfady1 <fadya5323@gmail.com>
2025-02-09 19:59:29 +0000

    انا عايز امووووووووت

--- FRONT-END/js/main.js
+++ FRONT-END/js/main.js
@@ -1,32 +1,5 @@
 "use strict";
-// Start DarkMode
-const body = document.querySelector("body");
-const toggle = document.querySelector("#toggle");
-const sunIcon = document.querySelector(".toggle .bxs-sun");
-const moonIcon = document.querySelector(".toggle .bx-moon");
 
-toggle.addEventListener("change", () => {
-  body.classList.toggle("dark");
-  sunIcon.className =
-    sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-  moonIcon.className =
-    moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-  if (body.classList.contains("dark")) {
-    localStorage.setItem("DarkToogle", "Dark");
-  } else {
-    localStorage.removeItem("DarkToogle");
-  }
-});
-document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("DarkToogle")) {
-    body.classList.add("dark");
-    sunIcon.className =
-      sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-    moonIcon.className =
-      moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-  }
-});
-// End DarkMode
 
 // Start Swiper Section 1
 const swiper = new Swiper(".swiper", {
@@ -151,3 +124,32 @@ var x = new Swiper(".categs", {
     },
   },
 });
+
+// Start DarkMode
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+
+toggle.addEventListener("change", () => {
+  body.classList.toggle("dark");
+  sunIcon.className =
+    sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+  moonIcon.className =
+    moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("DarkToogle", "Dark");
+  } else {
+    localStorage.removeItem("DarkToogle");
+  }
+});
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("DarkToogle")) {
+    body.classList.add("dark");
+    sunIcon.className =
+      sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className =
+      moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+  }
+});
+// End DarkMode
